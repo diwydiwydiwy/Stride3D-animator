ba854134e2eb1891adfd1a74fe4f06c99e9e198c
OctobuteR <68831111+OctobuteR@users.noreply.github.com>
2024-10-20 21:33:31 +0300

    The appearence of window within Stride editor
    But opening the window more than once is not possible

--- AnimatorExtensionTest/Animator.cs
+++ AnimatorExtensionTest/Animator.cs
@@ -7,18 +7,36 @@ using Stride.Core.Mathematics;
 using Stride.Input;
 using Stride.Engine;
 using AvaloniaApplicationForStride;
+using System.Reflection;
+using System.Diagnostics.Eventing.Reader;
+using Stride.Animations;
+using Avalonia.Controls;
+using Stride.Engine.Design;
 
 namespace AnimatorExtensionTest
 {
     public class Animator : StartupScript
     {
+        private Window _animatorWindow;
         private bool _showAnimator;
-        public bool OpenAnimator { get => _showAnimator; 
-                set 
-                {
-                    AvaloniaProgram.BuildAvaloniaApp().SetupWithoutStarting();
-                    new MainWindow().Show();
+        private bool _firstStart;
+        private bool FirstStart { get => _firstStart; set { _firstStart = value; } }
+        public bool OpenAnimator 
+        { 
+            get => _showAnimator; 
+            set 
+            {
+                AvaloniaProgram.BuildAvaloniaApp().SetupWithoutStarting();
+                FirstStart = false;
+                if (_animatorWindow != null) 
+                { 
+                    _animatorWindow.Close(); 
+                    _animatorWindow = null;
+                    return; 
                 }
+                _animatorWindow = new MainWindow();
+                _animatorWindow.Show();
+            }
         }
 
         public override void Start()
