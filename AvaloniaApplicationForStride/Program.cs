495d7db975cb3c537ba887a2945ac96ac6614fb6
OctobuteR <diwydiwydiwy@gmail.com>
2024-11-16 02:03:14 +0300

    Update avalonia window program class

--- AvaloniaApplicationForStride/Program.cs
+++ AvaloniaApplicationForStride/Program.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace AvaloniaApplicationForStride
 {
-    internal class Program
+    public class AvaloniaProgram
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
