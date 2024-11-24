b4bb3e2542ccce0fe601a0ea80915e7e6789d562
OctobuteR <diwydiwydiwy@gmail.com>
2024-11-24 23:00:55 +0300

    Split animator script and shift to EntityProcessor

---
+++ Scripts/AnimatorComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using AvaloniaApplicationForStride;
+using System.Reflection;
+using System.Diagnostics.Eventing.Reader;
+using Stride.Animations;
+using Avalonia.Controls;
+using Stride.Engine.Design;
+using Avalonia.Threading;
+using Stride.Games;
+using Stride.Core;
+using Stride.Rendering;
+using Stride.Core.Diagnostics;
+using System.ComponentModel;
+
+namespace AnimatorExtension
+{
+    [DataContract(nameof(AnimatorComponent))]
+    [ComponentCategory("Animator")]
+    [DefaultEntityComponentProcessor(typeof(AnimatorProcessor), ExecutionMode = ExecutionMode.Editor)]
+    public class AnimatorComponent: EntityComponent
+    {
+        private Window _animatorWindow;
+        [DataMemberIgnore] public Logger Log { get; set; }
+
+        public void InitializeAnimator() 
+        {
+            try
+            {
+                AvaloniaProgram.BuildAvaloniaApp().SetupWithoutStarting();
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Avalonia builder has stopped with an exception thrown: {e}");
+            }
+            finally 
+            {
+                _animatorWindow = new MainWindow();
+            }
+        }
+
+        public void OpenAnimator()
+        {
+            Log.Debug("Try opening");
+            if (!_animatorWindow.IsVisible)
+            {
+                Dispatcher.UIThread.Invoke(() => _animatorWindow.Show());
+                return;
+            }
+        }
+    }
+}
---
+++ Scripts/AnimatorProcessor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using AvaloniaApplicationForStride;
+using System.Reflection;
+using System.Diagnostics.Eventing.Reader;
+using Stride.Animations;
+using Avalonia.Controls;
+using Stride.Engine.Design;
+using Avalonia.Threading;
+using Stride.Games;
+using Stride.Core;
+using Stride.Rendering;
+using Stride.Core.Diagnostics;
+
+namespace AnimatorExtension
+{
+    public class AnimatorProcessor : EntityProcessor<AnimatorComponent>
+    {
+        private Logger _logger;
+        private Logger Log
+        {
+            get
+            {
+                if (_logger != null)
+                {
+                    return _logger;
+                }
+
+                var className = GetType().FullName;
+                _logger = GlobalLogger.GetLogger(className);
+                return _logger;
+            }
+        }
+        private InputManager Input { get; set; }
+        private static bool FirstStart { get; set; } = false;
+        public AnimatorProcessor(): base(typeof(AnimatorComponent)){ }
+        protected override void OnSystemAdd() 
+        {
+            Input = Services.GetSafeServiceAs<InputManager>();
+        }
+  
+        public override void Update(GameTime time)
+        {
+            foreach (var component in ComponentDatas.Values)
+            {
+                if (!FirstStart) 
+                {
+                    component.Log = Log;
+                    component.InitializeAnimator();
+                    FirstStart = true;
+                }
+                if (Input.IsKeyDown(Keys.LeftShift))
+                {
+                    if (Input.IsKeyPressed(Keys.F1))
+                    {
+                        component.OpenAnimator();
+                    }
+                }
+            }
+        }
+    }
+}
